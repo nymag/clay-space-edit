6545be14523b00de7f84a1a1905c1018c92894fe
Jon Winton <winton.jon@gmail.com>
2016-08-15 15:50:35 -0400

    fixing updated service name

--- controllers/space-controller.js
+++ controllers/space-controller.js
@@ -2,13 +2,14 @@ var dom = require('@nymag/dom'),
   _ = require('lodash'),
   kilnServices = window.kiln.services,
   references = kilnServices.references,
+  getAddableComponents = kilnServices.addComponentsHandler.getAddableComponents,
   spaceName = 'clay-space';
 
 function makeComponentListAttr(parent) {
   var include = _.get(parent, 'list.include') || _.get(parent, 'prop.include'),
   exclude = _.get(parent, 'list.exclude') || _.get(parent, 'prop.exclude');
 
-  return _.remove(kilnServices.getAddableComponents(include, exclude), function(component) {
+  return _.remove(getAddableComponents(include, exclude), function(component) {
     return component !== spaceName;
   });
 }
