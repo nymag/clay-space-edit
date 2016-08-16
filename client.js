e5b1018c01242b7c2c0dd01cde346a10accf1651
Jon Winton <winton.jon@gmail.com>
2016-08-16 16:27:48 -0400

    Removing controller

--- client.js
+++ client.js
@@ -2,12 +2,10 @@ var dom = require('@nymag/dom'),
   _ = require('lodash'),
   spaceName = 'clay-space',
   selector = require('./services/selector'),
-  SpaceController = require('./controllers/space-controller'),
-  SpaceLogicController = require('./controllers/space-logic-controller');
+  SpaceController = require('./controllers/space-controller');
 
 function updateSelector(el, options, parent) {
   var isSpaceComponent = el.classList.contains(spaceName),
-    isSpaceLogicComponent = el.classList.contains('space-logic'),
     parentIsSpaceLogic = parent.ref && parent.ref.indexOf('space-logic') > -1;
 
   if (_.get(parent, 'list.include', '') && _.contains(parent.list.include, spaceName) && !isSpaceComponent) {
@@ -20,10 +18,6 @@ function updateSelector(el, options, parent) {
 
   if (isSpaceComponent) {
     SpaceController(el, options, parent);
-  }
-
-  if (isSpaceLogicComponent) {
-    SpaceLogicController(el, options, parent);
   }
 }
 
