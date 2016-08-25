3cefa301e011f550fe804d32657a0f7471fa8707
Jon Winton <winton.jon@gmail.com>
2016-08-25 17:33:18 -0400

    Ammending how the save service updates

--- services/save-service.js
+++ services/save-service.js
@@ -1,15 +1,15 @@
 var dom = require('@nymag/dom'),
   kilnServices = window.kiln.services,
   edit = kilnServices.edit,
+  pane = kilnServices.pane,
   createService = require('./create-service'),
   selectorService = require('./selector'),
   editingClass = 'space-logic-editing';
 
 
 
-function onLogicSave(logic) {
-  var targetLogic = dom.find(document, `[data-uri="${logic._ref}"]`),
-    query = { currentUrl: window.location.href };
+function onLogicSave(logic, logicComponent) {
+  var query = { currentUrl: window.location.href };
 
   return edit.getHTMLWithQuery(logic._ref, query)
     .then(html => {
@@ -18,18 +18,26 @@ function onLogicSave(logic) {
           var newComponent = dom.find(document, `[data-uri="${logic._ref}"]`),
             addComponentButton;
 
-          // Replace the targetLogic with the new HTML
-          dom.replaceElement(targetLogic, html);
+          // Replace the logicComponent with the new HTML
+          dom.replaceElement(logicComponent, html);
           // TODO: Figure out why adding a class to `html` isn't persisting
           // when the replace is done. Shouldn't need to re-query the DOM
-          if (targetLogic.classList.contains(editingClass)) {
+          if (logicComponent.classList.contains(editingClass)) {
             newComponent.classList.add(editingClass);
           }
 
           addComponentButton = selectorService.revealAddComponentButton(newComponent);
 
-          this.addButtons();
+          this.updateChildrenCount()
+            .addButtons();
           addComponentButton.addEventListener('click', selectorService.launchAddComponent.bind(null, newComponent, { ref: this.spaceRef }, this.parent));
+
+          pane.close();
+
+          selectorService.launchBrowsePane(this.el, {
+            add: this.onAddCallback.bind(this),
+            remove: this.onRemoveCallback.bind(this)
+          });
         });
     });
 }
