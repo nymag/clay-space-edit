533c4b00a3489e28616506b5762dd7487b025458
Jon Winton <winton.jon@gmail.com>
2016-09-12 10:24:47 -0400

    Updating makeComponentListAttr to not include clay-space components

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,12 +1,20 @@
 var _ = require('lodash'),
   references = require('references');
 
+/**
+ * Return an array of components available in a
+ * component list that are not prefixed with `clay-space`,
+ * but include the `clay-space-edit` component
+ *
+ * @param  {Object} parent  The parent component which contains a componentList
+ * @return {Array}
+ */
 function makeComponentListAttr(parent) {
   var include = _.get(parent, 'list.include', '') || _.get(parent, 'prop.include', ''),
     exclude = _.get(parent, 'list.exclude', '') || _.get(parent, 'prop.exclude', '');
 
   return _.remove(references.availableComponents(include, exclude), function (component) {
-    return component !== references.spaceClass;
+    return component === references.spaceEdit || !_.startsWith(component, references.spacePrefix);
   });
 }
 
