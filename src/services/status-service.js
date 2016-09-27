ddf4cbe2bf4c7390607bf8bf496fa2dd048c1470
Jon Winton <winton.jon@gmail.com>
2016-09-27 15:52:50 -0400

    Adding some documentation

--- src/services/status-service.js
+++ src/services/status-service.js
@@ -6,44 +6,99 @@ var dom = require('@nymag/dom'),
   EDITING = 'data-logic-editing',
   LOGIC = 'data-logic';
 
+/**
+ * Add the active data attribute
+ *
+ * @param {Element} el
+ */
 function setActive(el) {
   el.setAttribute(ACTIVE, '');
 }
 
+/**
+ * Add the editing data attribute
+ *
+ * @param {Element} el
+ */
 function setEditing(el) {
   el.setAttribute(EDITING, '');
 }
 
+/**
+ * Add the active and editing data attribute
+ *
+ * @param {Element} el
+ */
 function setActiveAndEditing(el) {
   setEditing(el);
   setActive(el);
 }
 
+/**
+ * Remove the editing data attribute
+ *
+ * @param {Element} el
+ */
 function removeEditing(el) {
   el.removeAttribute(EDITING);
 }
 
+/**
+ * Remove the active data attribute
+ *
+ * @param {Element} el
+ */
 function removeActive(el) {
   el.removeAttribute(ACTIVE);
 }
 
+/**
+ * Remove the active and editing data attribute
+ *
+ * @param {Element} el
+ */
 function removeActiveAndEditing(el) {
   removeEditing(el);
   removeActive(el);
 }
 
+/**
+ * Find the first element with the active
+ * data attribute within an element
+ *
+ * @param {Element} el
+ * @return {Element}
+ */
 function findActive(el) {
   return dom.find(el, `[${ACTIVE}]`);
 }
 
+/**
+ * Test if a component has the active attribute
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ */
 function isActive(el) {
   return _.isString(el.getAttribute(ACTIVE));
 }
 
+/**
+ * Test if a component has the editing attribute
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ */
 function isEditing(el) {
   return _.isString(el.getAttribute(EDITING));
 }
 
+/**
+ * Test if a component has the logic attribute
+ *
+ * @param {Element} el
+ * @return {Boolean}
+ */
 function isLogic(el) {
   return _.isString(el.getAttribute(LOGIC));
 }
