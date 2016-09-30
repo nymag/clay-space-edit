0d073a6b992656510d29515f5f3068d7e6780386
Jon Winton <winton.jon@gmail.com>
2016-09-29 20:52:01 -0400

    Adding a better check for Spaces vs SpaceEdit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 var selector = require('./services/selector'),
   SpaceController = require('./controllers/space-controller'),
-  utils = require('./services/utils');
+  invisibleLists = require('./services/invisible-lists'),
+  utils = require('./services/utils'),
+  dom = require('@nymag/dom');
 
 // Load styles
 require('./styleguide/styles.scss');
 
 function updateSelector(el, options, parent) {
-  var isSpaceComponent = utils.checkIfSpace(options.ref),
-    availableSpaces = utils.spaceInComponentList(parent);
+  var isSpaceComponent, availableSpaces;
+
+  if (utils.checkIfSpaceEdit(options.ref)) {
+    return;
+  }
+
+  isSpaceComponent = utils.checkIfSpace(options.ref);
+  availableSpaces = utils.spaceInComponentList(parent);
 
   if (availableSpaces && availableSpaces.length > 0 && !isSpaceComponent) {
     selector.addAvailableSpaces(el, availableSpaces);
     selector.addCreateSpaceButton(el, options, parent);
     selector.stripSpaceFromComponentList(el);
+
   }
 
   if (isSpaceComponent) {
     selector.addAvailableSpaces(el, availableSpaces);
     SpaceController(el, parent);
     selector.addToComponentList(el, options, parent);
+
   }
 }
 
@@ -26,4 +36,5 @@ window.kiln = window.kiln || {};
 window.kiln.plugins = window.kiln.plugins || {};
 window.kiln.plugins['spaces-edit'] = function initSpaces() {
   window.kiln.on('add-selector', updateSelector);
+  window.kiln.on('component-pane:create-invisible-tab', invisibleLists);
 };
