cc2dfb39a172491d0034746110dc4b1ffb87cdbe
Amy Cheng <amy.cheng@nymag.com>
2018-07-11 14:11:04 -0400

    more test suite configuration

---
+++ test/utils.js
@@ -0,0 +1,41 @@
+const kilnApiStub = {
+    utils: {
+      components: {},
+      componentElements: {},
+      create: {},
+      references: {}
+    }
+  },
+  spaceSchema = {
+    content:{
+      _componentList: {
+        include: ['space-logic']
+      }
+    }
+  };
+
+
+export function VuexStoreStub(uiPath, dispatches) {
+  let cmptName, cmptSchema;
+
+  this.dispatch = (type, payload)=>{
+    dispatches.push({type, payload});
+    return Promise.resolve({});
+  };
+
+  this.state = {
+    schemas: {
+      'clay-space': spaceSchema
+    },
+    ui: {
+      currentSelection: {
+        parentField: {
+          uiPath
+        }
+      }
+    }
+  };
+};
+
+
+module.exports.kilnApiStub = kilnApiStub;
