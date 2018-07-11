f7a8de354d0e966854f07ca5b1aeed3f0bc93b64
Amy Cheng <amy.cheng@nymag.com>
2018-07-11 14:15:44 -0400

    more test config fiddling

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/services/*.js'],
+  coverageReporters: ['text-summary']
 };
--- test/dom-setup.js
+++
@@ -1,8 +0,0 @@
-//set-up a DOM environment for testing
-require('jsdom-global')();
-
-window.kiln = {
-  utils: {
-    create: {}
-  }
-};
